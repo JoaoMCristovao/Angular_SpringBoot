66287f559c9867e75a338c5c7110d5b8bb552a4a
João Santos <42850719+JoaoMariaSantos@users.noreply.github.com>
2025-03-11 00:07:46 +0000

    created angular frontend structure

---
+++ fibonacci-frontend/src/app/services/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiURL = 'http://localhost:8080/api/fibonacci';
+
+  constructor(private http: HttpClient) { }
+
+  //GET request to fetch single value
+  getFibonacciValue(n: number): Observable<number> {
+    return this.http.get<any>(`${this.apiURL}/${n}`).pipe(
+      map(response => response.output)
+    )
+  }
+}
